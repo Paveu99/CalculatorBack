bcc2457dff0dc2a36c602d13e5af1071fda53e7a
Pawel <pawel@example.com>
2023-02-22 13:27:39 +0100

    Added router

---
+++ index.ts
@@ -0,0 +1,22 @@
+import express, {json} from "express";
+import 'express-async-errors';
+import {handleError} from "./utils/errors";
+import './utils/db';
+import cors from 'cors';
+import {mathRouter} from "./routers/math";
+
+
+const app = express();
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
+app.use(json())
+
+app.use('/math', mathRouter);
+
+
+app.use(handleError);
+
+app.listen(3001, '0.0.0.0', () => {
+    console.log('Listening on http://localhost:3001');
+});
