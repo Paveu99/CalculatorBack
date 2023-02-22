4ac6e5d75cf19e613e9cea30bad1594732570c4a
Pawel <pawel@example.com>
2023-02-22 13:27:39 +0100

    Added records

---
+++ routers/math.ts
@@ -0,0 +1,38 @@
+import {Router} from "express";
+import {ValidationError} from "../utils/errors";
+import {CreateMathReq} from "../types";
+import {MathRecord} from "../records/math.record";
+
+
+export const mathRouter = Router();
+
+mathRouter
+
+    .get('/', async (req, res) => {
+        const mathHistory = await MathRecord.listAll();
+
+        res.json({
+            mathHistory,
+        });
+    })
+
+    .delete('/:id', async (req, res) => {
+        const expression = await MathRecord.getOne(req.params.id);
+
+        if (!expression) {
+            throw new ValidationError('No such expression')
+        }
+
+        await expression.delete()
+
+        res.end()
+    })
+
+    .post('/', async (req, res) => {
+        const newExpression = new MathRecord(req.body as CreateMathReq);
+        await newExpression.insert();
+
+        res.json({
+            newExpression,
+        })
+    });
